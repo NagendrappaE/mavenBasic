b44929573647ee8b9e6ea604914d6d7d6598ad50
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2023-05-15 16:54:21 +0530

    fix of fund trasfer

---
+++ Accountransfer/src/main/java/io/paycorp/Accountransfer/App.java
@@ -0,0 +1,25 @@
+package io.paycorp.Accountransfer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        try {
+			Connection conn = DriverManager.getConnection(
+			        "jdbc:mysql://127.0.0.1:3306/test", "root", "password");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
